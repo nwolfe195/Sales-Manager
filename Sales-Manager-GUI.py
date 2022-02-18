a3d69f01c98c5e3022554bc7f1e8aa2127bc1f36
nwolfe195 <nicholaswolfe195@gmail.com>
2022-02-17 16:14:17 -0800

    Some GUI fixes, next putting tables into tkinter display

--- Sales-Manager-GUI.py
+++ Sales-Manager-GUI.py
@@ -1,18 +1,28 @@
 import tkinter as tk
-from tkinter import *
 from tkinter import ttk
-from tkinter.messagebox import showinfo
+import sqlite3
+import pandas as pd
 
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
+        # Connect to database
+        self.conn = sqlite3.connect('Sales-Manager.db')
+        self.product_name_list = [tuple[0] for tuple in self.conn.execute('SELECT * FROM Product').description]
+        self.customers_name_list = [tuple[0] for tuple in self.conn.execute('SELECT * FROM Customer').description]
+        self.sales_name_list = [tuple[0] for tuple in self.conn.execute('SELECT * FROM Sales').description]
+        self.inventory_name_list = [tuple[0] for tuple in self.conn.execute('SELECT * FROM Inventory').description]
+
         # App configuration
         self.title('Sales Manager')
         tabControl = ttk.Notebook(self)
-        tabProduct = ttk.Frame(tabControl)
-        tabControl.add(tabProduct, text='Products')
+
+        self.tabProduct = ttk.Frame(tabControl)
+        self.tab_setup('Product')
+        tabControl.add(self.tabProduct, text='Products')
+
         tabCustomers = ttk.Frame(tabControl)
         tabControl.add(tabCustomers, text='Customers')
         tabSales = ttk.Frame(tabControl)
@@ -22,19 +32,23 @@ class App(tk.Tk):
         tabControl.pack(expand=1, fill='both')
 
 
+        #self.add_products('Product 1',10.00,'Unrestricted','oz','This is the first product')
+
+    def tab_setup(self, table):
+        sql_query = 'SELECT * FROM %s' % table
+        query = self.conn.execute(sql_query)
+        cols = [column[0] for column in query.description]
+        for i in range(len(cols)-1):
+            ttk.Label(self.tabProduct, text=cols[i+1]).grid(column=i, row=0)
+        data = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)
+        print(data)
+
+    def add_products(self, name, price, restrictions, unit, comment):
+        sql_insert = 'INSERT INTO Product (Name, Price, Restrictions, Unit, Comment) VALUES (\'%s\', %d, \'%s\', \'%s\', \'%s\')' % (name, price, restrictions, unit, comment)
+        self.conn.execute(sql_insert)
+        self.conn.commit()
+
+
 if __name__ == "__main__":
     app = App()
-    print('here?')
     app.mainloop()
-    print('there?')
-#root = tk.Tk()
-#root.title("Tab Widget")
-#tabControl = ttk.Notebook(root)
-#tab1 = ttk.Frame(tabControl)
-#tab2 = ttk.Frame(tabControl)
-#tabControl.add(tab1, text="Tab 1")
-#tabControl.add(tab2, text="Tab 2")
-#tabControl.pack(expand=1, fill="both")
-#ttk.Label(tab1, text="Welcome to Demo").grid(column=0, row=0, padx=30, pady=30)
-#ttk.Label(tab2, text="A second tab").grid(column=0, row=0, padx=30, pady=30)
-#root.mainloop()
